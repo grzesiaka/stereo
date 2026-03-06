4ef6d922b16d71aadc4de03c653dae4fd2776308
grzesiaka <artur@grzesiaka.pl>
2026-03-06 09:18:20 +0100

    vitest testing helpers

---
+++ libs/testing/src/index.ts
@@ -0,0 +1,69 @@
+import * as v from "vitest";
+
+type EQ = (<const A>(a: A) => (b: NoInfer<A>) => A) & (<const A>(a: A, b: NoInfer<A>) => A);
+
+const eq: EQ = (...a: [any] | [any, any]) => (a.length === 2 ? eq(a[0])(a[1]) : (b) => (v.expect(a[0]).toEqual(b), b));
+
+const ctx = {
+  /**
+   * `import * as v from "vitest"`
+   */
+  v,
+  /**
+   * Equality comparison that checks both for values and for types.
+   *
+   * ```
+   * type EQ = (<const A>(a: A) => (b: NoInfer<A>) => A) & (<const A>(a: A, b: NoInfer<A>) => A);
+   *
+   * const eq: EQ = (...a: [any] | [any, any]) => (a.length === 2 ? eq(a[0])(a[1]) : (b) => (v.expect(a[0]).toEqual(b), b));
+   * ```
+   */
+  eq,
+};
+
+type Ctx = typeof ctx;
+
+type TestFn<R = unknown> = (ctx: Ctx, vitest: typeof v) => R;
+
+/** A wrapper for vitest.test that auto-injects context with vitest export */
+export const test = <R = any>(n: string, fn: TestFn<R>) => v.test(n, () => fn(ctx, v));
+
+/**
+ * 
+ * A wrapper for `vitest.describe` to defining tests just a little bit faster
+ * 
+ * @param nameOrFn name of the test or function to be tested (name of the function becomes the name of the test )
+ * @param tests an object with tests; `_`s are replaced ` `; if starts with `ONLY` runs as `vitest.test.only`
+ * @param name allows override of the `nameOfFn`; if starts with `ONLY` runs as `vitest.describe.only`
+ * @returns void 0
+ *  
+ * ---- 
+ * 
+ * @self
+ * ```
+ * export const describe = (
+    nameOrFn: string | Function,
+    tests: (ctx: Ctx, vitest: typeof v) => Record<string, () => void>,
+    name = typeof nameOrFn === "string" ? nameOrFn : nameOrFn.name,
+  ) =>
+  (name.startsWith("ONLY") ? v.describe.only : v.describe)(name, () => {
+    const ts = tests(ctx, v);
+    for (const k in ts) {
+      (k.startsWith("ONLY") ? v.test.only : v.test)(k.replace(/_/g, " "), ts[k]);
+    }
+  });
+ * ```
+ */
+export const describe = (
+  nameOrFn: string | Function,
+  tests: (ctx: Ctx, vitest: typeof v) => Record<string, () => void>,
+  name: "ONLY" | (string & {}) = typeof nameOrFn === "string" ? nameOrFn : nameOrFn.name,
+) =>
+  (name.startsWith("ONLY") ? v.describe.only : v.describe)(name, () => {
+    const ts = tests(ctx, v);
+    for (const k in ts) {
+      (k.startsWith("ONLY") ? v.test.only : v.test)(k.replace(/_/g, " "), ts[k]);
+    }
+  });
+
+export default describe;
