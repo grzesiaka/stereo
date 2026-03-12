8138e6b9091446f8f8bd65a19af45ecda4736e20
grzesiaka <artur@grzesiaka.pl>
2026-03-12 11:08:32 +0100

    arrobje seed

---
+++ libs/arrobje/src/index.ts
@@ -0,0 +1,8 @@
+// import type { ARR } from "~types";
+
+// export type $ArrObje<
+//   Xs extends ARR,
+//   Key extends keyof Xs[number],
+//   Value extends keyof Xs[number] | undefined = undefined,
+//   ObjeSym extends string = "$",
+// > = 1;
