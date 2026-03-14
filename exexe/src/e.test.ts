dc67cce5abd849e45f2225d4f4f943d1ac2262af
grzesiaka <artur@grzesiaka.pl>
2026-03-14 13:20:56 +0100

    simple event & input emitter

---
+++ exexe/src/e.test.ts
@@ -0,0 +1,52 @@
+import { describe } from "~testing";
+
+import EM from "./e";
+
+describe(EM, ({ eq, res }) => ({
+  no_context: () => {
+    const { I, OO } = EM<number>()();
+    const r = res<number>();
+
+    I(0);
+    const d = OO(r.add);
+    I(1);
+    I(2);
+    d();
+    I(3);
+    r.eq([1, 2]);
+  },
+
+  L_context: () => {
+    const { I, OO } = EM<number>()("L");
+
+    eq(I.I, "L");
+    eq(OO.OO, "L");
+
+    const r = res<number>();
+
+    I(0);
+    const d = OO(r.add);
+    I(1);
+    I(2);
+    d();
+    I(3);
+    r.eq([1, 2]);
+  },
+
+  LR_context: () => {
+    const { I, OO } = EM<number>()("L", "R");
+
+    eq(I.I, "L");
+    eq(OO.OO, "R");
+
+    const r = res<number>();
+
+    I(0);
+    const d = OO(r.add);
+    I(1);
+    I(2);
+    d();
+    I(3);
+    r.eq([1, 2]);
+  },
+}));
