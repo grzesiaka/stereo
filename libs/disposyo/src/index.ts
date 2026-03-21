d707c331a26a74130632fe253f73166b424477ae
grzesiaka <artur@grzesiaka.pl>
2026-03-21 21:39:14 +0100

    use ifArray in disposyo and fix types

--- libs/disposyo/src/index.ts
+++ libs/disposyo/src/index.ts
@@ -1,17 +1,23 @@
 import { type Tree } from "treeo/types";
 import forEach from "treeo/map";
-import { ARR, OP } from "~js";
+import { ARR, OP } from "~js/index";
+// import iA from "~js/if/if-array"; // TODO - tsdown does not emit types for the default export :/
+import { ifArray } from "~js";
 
 export type Dispose = () => void;
 
-type DisposeOrArrayOfDispose = Dispose | ARR<Dispose>;
-
-export type Tree_of_Disposable = Tree<DisposeOrArrayOfDispose>;
+export type Tree_of_Disposable = Tree<Dispose | ARR<Dispose>>;
 
 export type Disposyo<T extends Tree_of_Disposable = Tree_of_Disposable> = Dispose & { __: OP<"0", T> };
 
 const dispose = (T: Tree_of_Disposable) =>
-  forEach(T)(([v]) => (Array.isArray(v) ? v.forEach((d) => d()) : (v as Dispose)())); // TODO: mapTree types are bit of
+  forEach(T)(([v]) =>
+    ifArray(
+      v,
+      (a) => a.forEach((c) => c()),
+      (v) => v(),
+    ),
+  );
 
 export const disposyo = <T extends Tree_of_Disposable = ARR<Dispose>>(T = [] as unknown as T): Disposyo<T> => {
   const $: Disposyo<T> = OP("0")(T)(() => dispose($.__[1]));
