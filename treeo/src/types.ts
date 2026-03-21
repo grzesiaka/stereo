7cc5da25379316fb173d85cd2240b68027e170b6
grzesiaka <artur@grzesiaka.pl>
2026-03-21 21:38:47 +0100

    treeo fix kv of most generic tree (v was unknown; now is from tree definition)

--- treeo/src/types.ts
+++ treeo/src/types.ts
@@ -1,7 +1,8 @@
 import type * as TF from "type-fest";
 import { ARR, Fn, Fn0 } from "~js";
 
-export type Tree<X = unknown> = X | { [k in string]: Tree<X> };
+export type Tree<X = unknown> = X | { [k: string]: Tree<X> };
+export type Tree$X<T> = T extends Tree<infer X> ? X : never;
 
 export type Tree_of_Functions<Input extends ARR = ARR, Output = unknown> = Tree<Fn<Input, Output>>;
 export type Tree_of_Thunks<Output = unknown> = Tree<Fn0<Output>>;
@@ -12,11 +13,10 @@ export type Tree_of_Functions$Tree_of_Outputs<T> = T extends { readonly [K in st
     ? ReturnType<T>
     : never;
 
-export type Tree$ValueKeyPairs<T> = { [K in TF.Paths<T, { leavesOnly: true }> & string]: [TF.Get<T, K>, K] }[TF.Paths<
-  T,
-  { leavesOnly: true }
-> &
-  string];
+export type Tree$ValueKeyPairs<T> = { [s: string]: Tree<any> } extends T
+  ? [Tree$X<T>, string]
+  : { [K in TF.Paths<T, { leavesOnly: true }> & string]: [TF.Get<T, K>, K] }[TF.Paths<T, { leavesOnly: true }> &
+      string];
 
 export type MapTree<T, X> = T extends Fn
   ? X
