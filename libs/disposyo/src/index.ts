f4bf355628566157c9d820b39a139b4ac8b04f87
grzesiaka <artur@grzesiaka.pl>
2026-03-21 14:24:48 +0100

    defaults for disposyo

--- libs/disposyo/src/index.ts
+++ libs/disposyo/src/index.ts
@@ -4,16 +4,16 @@ import { ARR, OP } from "~js";
 
 export type Dispose = () => void;
 
-type Leaf = Dispose | ARR<Dispose>;
+type DisposeOrArrayOfDispose = Dispose | ARR<Dispose>;
 
-export type Tree_of_Disposable = Tree<Leaf>;
+export type Tree_of_Disposable = Tree<DisposeOrArrayOfDispose>;
 
-export type Disposyo<T extends Tree_of_Disposable> = Dispose & { __: OP<"0", T> };
+export type Disposyo<T extends Tree_of_Disposable = Tree_of_Disposable> = Dispose & { __: OP<"0", T> };
 
 const dispose = (T: Tree_of_Disposable) =>
   forEach(T)(([v]) => (Array.isArray(v) ? v.forEach((d) => d()) : (v as Dispose)())); // TODO: mapTree types are bit of
 
-export const disposyo = <T extends Tree_of_Disposable>(T: T): Disposyo<T> => {
+export const disposyo = <T extends Tree_of_Disposable = ARR<Dispose>>(T = [] as unknown as T): Disposyo<T> => {
   const $: Disposyo<T> = OP("0")(T)(() => dispose($.__[1]));
   return $;
 };
