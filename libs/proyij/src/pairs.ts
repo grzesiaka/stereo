019f88a3f02736a19233d214ff1b212969d86001
grzesiaka <artur@grzesiaka.pl>
2026-03-28 14:09:39 +0100

    proiy to object works with arrays

--- libs/proyij/src/pairs.ts
+++ libs/proyij/src/pairs.ts
@@ -1,11 +1,11 @@
-import { Simplify } from "type-fest";
+import { Simplify, UnionToIntersection } from "type-fest";
 import { ARR } from "~types";
 
-export type _KeyValues$Object<Xs> = Xs extends readonly [[infer K, infer V], ...infer R]
-  ? (K extends PropertyKey ? { [k in K]: V } : {}) & _KeyValues$Object<R>
-  : {};
-
-export type KeyValues$Object<Xs> = Simplify<_KeyValues$Object<Xs>>;
+export type KeyValues$Object<Xs extends ARR<[PropertyKey, unknown]>> = Simplify<
+  UnionToIntersection<
+    Xs[number] extends [PropertyKey, any] ? { [k in Xs[number][0]]: Extract<Xs[number], [k, any]>[1] } : {}
+  >
+>;
 
 const toObject = <X extends ARR<[PropertyKey, unknown]>>(x: X) =>
   x.reduce((a, [k, v]) => (((a as any)[k] = v), a), {} as KeyValues$Object<X>);
