2a9c9c7790cb59a415d0456962d6a677a728b3c8
grzesiaka <artur@grzesiaka.pl>
2026-03-29 11:01:52 +0200

    treeo: mute ts issue

--- treeo/src/map.ts
+++ treeo/src/map.ts
@@ -5,7 +5,7 @@ export const map =
   <X>(f: (vk: Tree$ValueKeyPairs<T>) => X): MapTree<T, X> => {
     if (typeof t === "object" && !Array.isArray(t) && t !== null) {
       return Object.entries(t).reduce((a, [k, v]) => {
-        a[k] = map(v, ks ? `${ks}.${k}` : k)(f);
+        a[k] = map(v, ks ? `${ks}.${k}` : k)(f as any);
         return a;
       }, {} as any);
     } else {
