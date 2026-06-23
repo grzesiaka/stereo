7c6f4d0c037bdac44d3633be54dc76bb7f5090bd
grzesiaka <artur@grzesiaka.pl>
2026-06-23 12:46:22 +0200

    xpresyo: commandify (WIP)

--- libs/xpresyo/src/commandify.ts
+++ libs/xpresyo/src/commandify.ts
@@ -1,9 +1,32 @@
-import { map, Tree_of_Functions } from "treeo";
-import { Fn, Fn$I } from "~types";
+import type { ARR } from "jsyoyo";
+import { map, Tree_of_Functions, Tree$Values } from "treeo";
+import { Fn, Fn$I, Fn$O_Recursive } from "~types";
+
+export type Command<T> =
+  Fn$O_Recursive<Tree$Values<T>> extends readonly [infer K, infer Ps, any] ? [K, Ps, ARR<Command<T>>] : T;
+
+type CommandLeafBase<K extends string = string, Params extends ARR = ARR> = [K, Params];
+type CommandBase<
+  K extends string = string,
+  Params extends ARR = ARR,
+  Kids extends ARR<CommandLeafBase | CommandBase> = ARR<any>,
+> = [K, Params, Kids] | CommandLeafBase;
 
 export type CommandifyLeaves<T, P extends string = ""> = T extends Fn
   ? <I extends Fn$I<T>>(...I: I) => [P, I]
-  : { [K in keyof T & (string | number)]: CommandifyLeaves<T[K], "" extends P ? `${K}` : `${P}.${K}`> };
+  : { [K in keyof T & string]: CommandifyLeaves<T[K], "" extends P ? `${K}` : `${P}.${K}`> };
 
 export const commandifyLeaves = <T extends Tree_of_Functions>(fns: T) =>
   map(fns)(([_, k]) => (...ps: any) => [k, ps]) as CommandifyLeaves<T>;
+
+export type Commandify<T, Constraint, P extends string = ""> = T extends Fn
+  ? <I extends Fn$I<T>>(...I: I) => <const K extends ARR<Constraint>>(...kids: K) => [P, I, K]
+  : { [K in keyof T & string]: Commandify<T[K], Constraint, "" extends P ? `${K}` : `${P}.${K}`> };
+
+export const $commandify =
+  <KidsConstraint>() =>
+  <T extends Tree_of_Functions>(fns: T) =>
+    map(fns)(([_, k]) => (...ps: any[]) => (...kids: any[]) => [k, ps, kids]) as Commandify<T, KidsConstraint>;
+export const commandify = $commandify<CommandBase>();
+
+export default commandify;
--- libs/xpresyo/test/commandify.test.ts
+++ libs/xpresyo/test/commandify.test.ts
@@ -1,6 +1,6 @@
 import { describe } from "~testing";
 
-import { commandifyLeaves } from "../src/commandify";
+import { commandifyLeaves, commandify, $commandify, Command } from "../src/commandify";
 
 describe(commandifyLeaves, ({ eq }) => ({
   empty: () => {
@@ -8,8 +8,8 @@ describe(commandifyLeaves, ({ eq }) => ({
     eq(F, {});
   },
   single: () => {
-    const F = (x: "A") => [x];
-    eq(F("A"), ["A"]);
+    const F = commandifyLeaves((a: "A") => [a]);
+    eq(F("A"), ["", ["A"]]);
   },
   object: () => {
     const F = commandifyLeaves({ a: (x: "A") => [x], b: (x: "B") => [x] });
@@ -27,5 +27,44 @@ describe(commandifyLeaves, ({ eq }) => ({
     eq(F.a("A"), ["a", ["A"]]);
     eq(F.b("B"), ["b", ["B"]]);
     eq(F.c.d("D"), ["c.d", ["D"]]);
+  },
+}));
+
+describe(commandify, ({ eq }) => ({
+  empty: () => {
+    const F = commandify({});
+    eq(F, {});
+  },
+  single: () => {
+    const F = $commandify<number>()((a: "A") => [a]);
+    eq(F("A")(1, 2, 3), ["", ["A"], [1, 2, 3]]);
+  },
+  object: () => {
+    const F = commandify({ a: (x: "A") => [x], b: (x: "B") => [x] });
+    eq(F.a("A")(F.b("B")()), ["a", ["A"], [["b", ["B"], []]]]);
+  },
+  nested: () => {
+    const F = commandify({
+      a: (x: "A") => [x],
+      b: (x: "B") => [x],
+      c: {
+        d: (x: "D") => [x],
+      },
+    });
+
+    eq(F.a("A")(), ["a", ["A"], []]);
+    eq(F.b("B")(), ["b", ["B"], []]);
+    eq(F.c.d("D")(), ["c.d", ["D"], []]);
+
+    const r = [
+      "c.d",
+      ["D"],
+      [
+        ["a", ["A"], []],
+        ["b", ["B"], []],
+      ],
+    ] as const satisfies Command<typeof F>;
+
+    eq(F.c.d("D")(F.a("A")(), F.b("B")()), r);
   },
 }));
