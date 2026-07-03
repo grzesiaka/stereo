697e7297559d58709aec2b5ab439d798e6a32505
grzesiaka <artur@grzesiaka.pl>
2026-07-03 12:12:50 +0200

    xpresyo: simplify test ast

--- libs/xpresyo/test/expressify.test.ts
+++ libs/xpresyo/test/expressify.test.ts
@@ -28,8 +28,10 @@ describe(expressify, ({ eq }) => ({
     });
 
     const $ = cmd.$;
+    const C = $.C(),
+      R = $.R();
     const T = cmd.Txt;
-    const ast = $.R()($.C()(T("Head1"), T("Bla")), $.C()(T("Head2"), T("Bla 2")));
+    const ast = R(C(T("Head1"), T("Bla")), C(T("Head2"), T("Bla 2")));
     const r = run(ast);
     const c2 = r.__[2][1];
     eq(c2.type, "COL");
