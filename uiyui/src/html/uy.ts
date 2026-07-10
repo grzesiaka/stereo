44d1db2a85a27eef8bf7fbb11498b09727d8ece0
grzesiaka <artur@grzesiaka.pl>
2026-07-10 17:15:47 +0200

    uiyui: fix ts2589 when gettting elements by_id

--- uiyui/src/html/uy.ts
+++ uiyui/src/html/uy.ts
@@ -1,11 +1,9 @@
-import { dethunk } from "jsyoyo";
+import { dethunk, NestedArrays$ValuesUnion } from "jsyoyo";
 import { $el, HTML_AST, HTML_Tag, HTML_Props, uyElement } from "./el";
-
-// type HTML = HTML_Props | readonly [HTML_Props, ARR<HTML>?];
 
 type InitHTMLs<Kids> = Kids extends readonly [infer H, ...infer R] ? [InitHTML<H>, ...InitHTMLs<R>] : [];
 
-type InitHTML<AST> = AST extends (...a: any[]) => readonly [infer T, infer P extends HTML_Props<any>, any?]
+export type InitHTML<AST> = AST extends (...a: any[]) => readonly [infer T, infer P extends HTML_Props<any>, any?]
   ? InitHTML<[T, P]>
   : AST extends [infer T extends HTML_Tag, infer P extends HTML_Props<any>, infer Kids]
     ? [uyElement<T, P>, InitHTMLs<Kids>]
@@ -13,9 +11,11 @@ type InitHTML<AST> = AST extends (...a: any[]) => readonly [infer T, infer P ext
       ? uyElement<T, P>
       : never;
 
-type BY_ID_ARR<Arr> = Arr extends readonly [infer H, ...infer R] ? BY_ID<H> & BY_ID_ARR<R> : {};
-type BY_ID1<E> = E extends { id: infer Id extends string } ? { [id in Id]: E } : {};
-type BY_ID<HTML> = HTML extends readonly [infer E, infer R] ? BY_ID1<E> & BY_ID_ARR<R> : BY_ID1<HTML>;
+export type BY_ID<T> = {
+  [Node in NestedArrays$ValuesUnion<T, { id: string }> as Node extends { id: infer Id extends string }
+    ? Id
+    : never]: Node;
+};
 
 const _init = <AST extends HTML_AST>(ast: AST, ids = {} as Partial<BY_ID<InitHTML<AST>>>): InitHTML<AST> => {
   const [t, p, ks] = dethunk(ast);
@@ -28,7 +28,7 @@ const _init = <AST extends HTML_AST>(ast: AST, ids = {} as Partial<BY_ID<InitHTM
   const e = $el(t)(p);
   // @ts-expect-error
   e.id && (ids[e.id] = e);
-  const ch = ks.map((k) => _init(k, ids));
+  const ch = ks.map((k) => _init(k as AST, ids));
   (e as never as HTMLElement).append(...(ch as never as HTMLElement[]));
   return [e, ch] as InitHTML<AST>;
 };
