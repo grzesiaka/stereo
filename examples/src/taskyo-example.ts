6698a42a181ad99603ef616d0c467cd94c3caccf
grzesiaka <artur@grzesiaka.pl>
2026-09-16 10:56:30 +0200

    repo: change ignored pattern in scripts

--- examples/src/taskyo-example.ts
+++ examples/src/taskyo-example.ts
@@ -1,20 +1,20 @@
-// oxlint-disable no-undef
+// // oxlint-disable no-undef
 
-// import "taskyo/cache/index-db";
+// // import "taskyo/cache/index-db";
 
-import "taskyo/cache/memory";
-import { task, CACHE } from "taskyo";
-import type { CacheRegistry } from "taskyo";
+// import "taskyo/cache/memory";
+// import { task, CACHE } from "taskyo";
+// import type { CacheRegistry } from "taskyo";
 
-const r = {} as CacheRegistry;
+// const r = {} as CacheRegistry;
 
-const t = task.$<1, 2>(() => 2)({
-  Id: "",
-  cache: {
-    key: (p, d) => `${p}`,
-    store: "memory",
-    ttl: 1,
-  },
-});
+// const t = task.$<1, 2>(() => 2)({
+//   Id: "",
+//   cache: {
+//     key: (p, d) => `${p}`,
+//     store: "memory",
+//     ttl: 1,
+//   },
+// });
 
-console.log(CACHE.registry as CacheRegistry, t);
+// console.log(CACHE.registry as CacheRegistry, t);
