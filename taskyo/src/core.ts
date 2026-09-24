d0ed08e13f5242537b31e36b5018ab304ed2e651
grzesiaka <artur@grzesiaka.pl>
2026-09-24 09:41:09 +0200

    taskyo (WIP)

--- taskyo/src/core.ts
+++ taskyo/src/core.ts
@@ -1,32 +1,24 @@
-import { __, ARR, dethunk } from "jsyoyo";
-import { Remove, remove } from "arryo";
+import { __, ARR, dethunk, Json } from "jsyoyo";
+// import { Remove, remove } from "arryo";
 import { ERRs } from "rrerrorr";
-import { Tree, Dethunk, awaiT } from "treeo";
-
-type INIT = Tree<() => Promise<unknown>>;
-type INIT$CTX<I extends __<INIT>> = __ extends I ? __ : Dethunk<I>;
+import { Tree, Dethunk, awaiT, AwaiTreed } from "treeo";
 
 type ERR_COMPAT = Error | { $: Error };
 type ERR_SPEC = ARR<ERR_COMPAT>;
 
+type INIT = Tree<Json | (() => Json) | (() => Promise<unknown>)> & { ERR?: () => Promise<ERR_SPEC> };
+type INIT$CTX<I extends __<INIT>> = __ extends I ? __ : AwaiTreed<Dethunk<I>>;
+
 export const init = awaiT.$(dethunk) as <T extends INIT>(d: T) => INIT$CTX<T>;
 
-type SPEC$RunFn<Params = unknown, Err extends __<ERR_SPEC> = __, Ctx = __> = (
-  ...args: Remove<[params: Params, ctx: Ctx, err: Err], __>
-) => unknown;
-
-export interface SPEC<Params = unknown, Err extends __<ERR_SPEC> = __, Init extends __<INIT> = __> {
+export interface SPEC<Params = unknown, Init extends __<INIT> = __> {
   init?: Init;
   ctx?: INIT$CTX<Init>;
   params?: Params;
-  err?: Err;
-  run?: SPEC$RunFn<Params, Err, INIT$CTX<Init>>;
+  run?: (params: NoInfer<Params>, ctx: INIT$CTX<Init>) => unknown;
 }
 
 export const spec = <S extends SPEC>(s: S) => s;
-
-const s = spec({ a: 1, params: {} as { a?: 1 }, run: (a) => 1 });
-const r = null as any as SPEC$RunFn<typeof s>;
 
 export interface RUN<S extends SPEC> {
   spec: S;
@@ -42,7 +34,3 @@ export const ERR = ERRs(($) => ({
     },
   },
 }))["taskyo"]["err"];
-
-ERR.retry();
-
-ERR.timeout(1);
--- taskyo/test/index.test.ts
+++ taskyo/test/index.test.ts
@@ -1,11 +1,25 @@
 import { describe } from "~testing";
-import { ERR } from "../src";
+import { ERR, init } from "../src";
+
+import * as jsyoyo from "jsyoyo";
+
+import j from "jsyoyo/_dyn";
 
 describe("", ({ eq }) => ({
+  init: async () => {
+    const ctx = await init({
+      a: () => Promise.resolve(1),
+      b: { bb: () => "b.bb" },
+      json: [],
+      jsyoyo: j,
+    });
+    eq(ctx, { a: 1, b: { bb: "b.bb" }, json: [], jsyoyo });
+  },
   ERR: () => {
     eq(ERR.abort() instanceof Error, true);
     const e = new ERR.abort.$("ctx");
     eq(e instanceof Error, true);
+    eq(e instanceof ERR.abort.$, true);
     eq(e.name, "taskyo.err.abort");
     eq(e.ctx, ["ctx"]);
   },
